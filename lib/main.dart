0088b89d46f0aa57f51ef42b571bf7c2d914cb3e
saraayy <128136969+saraayy@users.noreply.github.com>
2026-02-26 09:28:12 +0200

    Refactor landing screen connection UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:even_realities_g1/even_realities_g1.dart';
 import 'package:flutter/material.dart';
-import 'package:front/services/lc3_decoder.dart';
-import 'package:front/services/audio_pipeline.dart';
-import 'services/websocket_service.dart';
 import 'screens/landing_screen.dart';
 
 void main() {
@@ -10,32 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final G1Manager? manager;
-  final WebsocketService? ws;
-  final Lc3Decoder? decoder;
-  final AudioPipeline? audioPipeline;
+  const MyApp({super.key});
 
-  const MyApp(
-      {this.manager, this.ws, this.decoder, this.audioPipeline, super.key});
-
-/*
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: LandingScreen(),
-    );
-  }
-}
-*/
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LandingScreen(
-        manager: manager,
-        ws: ws,
-        decoder: decoder,
-        audioPipeline: audioPipeline,
-      ),
     );
   }
 }
