85ad69f0664e3dacd652bad6c23f65d42e8a749d
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-03-09 17:12:26 +0200

    fix: format

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart' as fbp;
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   fbp.FlutterBluePlus.setLogLevel(fbp.LogLevel.none,
-      color: false);  // Disable logging for fbp package
+      color: false); // Disable logging for fbp package
   runApp(const MyApp());
 }
 
