63365f4bf646f309f8f7253914ea9b7fdcd5038d
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-03-09 16:57:31 +0200

    fix: edit comment
    Co-authored-by: vainiovesa <186166946+vainiovesa@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart' as fbp;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  fbp.FlutterBluePlus.setLogLevel(fbp.LogLevel.none,
-      color: false); // lokitus bännäyksen poisto terminalista
-
+  fbp.FlutterBluePlus.setLogLevel(fbp.LogLevel.none, color: false);  // Disable logging
   runApp(const MyApp());
 }
 
